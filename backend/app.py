f8ea7673c8be37df488f39d3e464dc49497703d5
Divakardii <123715971+Divakardii@users.noreply.github.com>
2026-03-05 13:42:38 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/api')
+@app.route('/')
 def home():
-    return jsonify({"message": "Backend running successfully using Python Flask"})
+    return "Backend is running successfully!"
 
-if __name__ == '__main__':
+@app.route('/api')
+def api():
+    return {"message": "Hello from backend"}
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
